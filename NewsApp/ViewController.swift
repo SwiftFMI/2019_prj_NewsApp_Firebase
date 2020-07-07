f77436807ba667c4004208706c99f21ed85e460d
Nikoleta Yankova <yankovan@vmware.com>
2020-07-07 22:49:56 +0300

    Creates second screen with default values and basic navigation controller

--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -20,7 +20,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.title = "Main Controller"
     }
     
 }
